b6afa03b8c155e9234cb5f8196ca80da29710f94
swbhoi <swbhoi@gmail.com>
2016-11-02 06:15:21 -0400

    commited the api code

---
+++ test114/src/gen/java/io/swagger/api/NotFoundException.java
@@ -0,0 +1,10 @@
+package io.swagger.api;
+
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JaxRSServerCodegen", date = "2016-11-02T06:15:21.136-04:00")
+public class NotFoundException extends ApiException {
+	private int code;
+	public NotFoundException (int code, String msg) {
+		super(code, msg);
+		this.code = code;
+	}
+}
