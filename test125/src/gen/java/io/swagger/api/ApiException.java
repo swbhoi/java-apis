e2e10c218e78ba70837d28d8972ad2200bce461c
swbhoi <swbhoi@gmail.com>
2016-11-04 03:10:27 -0400

    commited the api code

---
+++ test125/src/gen/java/io/swagger/api/ApiException.java
@@ -0,0 +1,10 @@
+package io.swagger.api;
+
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JaxRSServerCodegen", date = "2016-11-04T03:10:26.621-04:00")
+public class ApiException extends Exception{
+	private int code;
+	public ApiException (int code, String msg) {
+		super(msg);
+		this.code = code;
+	}
+}
