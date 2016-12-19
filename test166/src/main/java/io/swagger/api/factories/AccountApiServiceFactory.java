000c80e5b724d47c909d9b8d261ca228ec22fb7e
swbhoi <swbhoi@gmail.com>
2016-12-19 08:28:33 -0500

    commited the api code

---
+++ test166/src/main/java/io/swagger/api/factories/AccountApiServiceFactory.java
@@ -0,0 +1,15 @@
+package io.swagger.api.factories;
+
+import io.swagger.api.AccountApiService;
+import io.swagger.api.impl.AccountApiServiceImpl;
+
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JaxRSServerCodegen", date = "2016-12-19T08:28:32.759-05:00")
+public class AccountApiServiceFactory {
+
+   private final static AccountApiService service = new AccountApiServiceImpl();
+
+   public static AccountApiService getAccountApi()
+   {
+      return service;
+   }
+}
