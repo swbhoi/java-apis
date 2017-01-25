9c2dc638c96ce6982378580c447716a8a574bf71
swbhoi <swbhoi@gmail.com>
2017-01-25 06:40:21 -0500

    commited the api code

---
+++ mytest6/src/gen/java/io/swagger/api/AccountApiService.java
@@ -0,0 +1,27 @@
+package io.swagger.api;
+
+import io.swagger.api.*;
+import io.swagger.model.*;
+
+import com.sun.jersey.multipart.FormDataParam;
+
+import io.swagger.model.Account;
+
+import java.util.List;
+import io.swagger.api.NotFoundException;
+
+import java.io.InputStream;
+
+import com.sun.jersey.core.header.FormDataContentDisposition;
+import com.sun.jersey.multipart.FormDataParam;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
+
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JaxRSServerCodegen", date = "2017-01-25T06:40:21.200-05:00")
+public abstract class AccountApiService {
+  
+      public abstract Response accountAccountNumberGet(SecurityContext securityContext)
+      throws NotFoundException;
+  
+}
